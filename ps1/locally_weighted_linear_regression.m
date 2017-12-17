0579e11a6992894cba12cd12c8b5965b25680fd3
p1p13 <piyushanandismite@gmail.com>
2017-12-17 15:59:12 +0530

    REctified code for LWLR

--- ps1/locally_weighted_linear_regression.m
+++ ps1/locally_weighted_linear_regression.m
@@ -2,11 +2,12 @@
 
 function[Y2] = locally_weighted_linear_regression(X, Y, tau)
 n = length(X);
-X = [ones(n,1), X];
+X2 = [ones(n,1), X];
 Y2 = zeros(n,1);
 
 for i = 1:n
-	W = exp(-(X - X(i))) .^ 2 / (2 * tau^2);
-	theta =  (X' * (X .* W))) \ (X' * (W .* Y));
-	Y2(i) = X(i) * theta;
+	W = exp(-(X - X(i)) .^ 2 / (2 * tau^2));
+	theta =  (X2' * (X2 .* W)) \ (X2' * (W .* Y));
+	Y2(i) = X2(i, :) * theta;
+	i;
 end 
